96f5b5a020ad8785e9bd0a34c5d8fc828804c830
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-25 12:02:13 +0200

    added more tag in  JXS that render method returns

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <>
+        <h1>Hello World!</h1>
+        <p>I am </p>
+      </>
+    );
   }
 }
 export default App;
