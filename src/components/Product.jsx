67a4bc31e0442d76c54cfc7bb1d3fafa14091dca
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-25 15:40:41 +0200

    used map method for my list const
    map method for converting an array of strings to an array of element

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,14 +2,22 @@ import { Component } from 'react';
 
 export default class Product extends Component {
   render() {
+    // const count = 2;
+    const list = ['Item one ', 'Item two', 'Item three'];
+    //map method for converting an array of strings to an array of element
+    const listmap = list.map((item) => <li>item</li>);
     return (
       <>
         <span className="m-2 text-info">Product Name</span>
-        <span className="m-2 badge bg-primary">2</span>
+        <span className="m-2 badge bg-primary">{this.format()}</span>
         <button className="m-2 btn btn-sm btn-success">+</button>
         <button className="m-2 btn btn-sm btn-warning">-</button>
         <button className="m-2 btn btn-sm btn-danger">Delete</button>
+        <ul>{listmap}</ul>
       </>
     );
+  }
+  format() {
+    return 2 + 2;
   }
 }
