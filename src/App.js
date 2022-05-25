e936b09f0a40f360adc170449ee1521e5a3c5a60
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-25 12:43:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-// import { Component } from 'react';
+import { Component } from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <>
-//         <h1>Hello World!</h1>
-//         <p>I am </p>
-//       </>
-//     );
-//   }
-// }
-
-function App() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-      <p>I am </p>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello World!</h1>
+        <p>I am </p>
+      </>
+    );
+  }
 }
 
 export default App;
