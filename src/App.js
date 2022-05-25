491a66400fe62de855ea879e599e30492094f495
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-25 12:05:21 +0200

    refactored and used functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <h1>Hello World!</h1>
-        <p>I am </p>
-      </>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <h1>Hello World!</h1>
+//         <p>I am </p>
+//       </>
+//     );
+//   }
+// }
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>I am </p>
+    </>
+  );
 }
+
 export default App;
