fe43c6780776709cf98bf52135a9712757a6f76a
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-25 11:49:02 +0200

    created a class base component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
+import App from './App';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello World</h1>;
-ReactDOM.render(element, document.getElementById('root'));
+// const element = <h1>Hello World</h1>;
+ReactDOM.render(<App />, document.getElementById('root'));
