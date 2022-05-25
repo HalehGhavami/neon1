262bd8f28928b4b8ac294651625fba93165775eb
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-25 12:43:30 +0200

    imported css from bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import App from './App';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-// const element = <h1>Hello World</h1>;
 ReactDOM.render(<App />, document.getElementById('root'));
