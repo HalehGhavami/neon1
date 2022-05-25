0ddd2f19dc7c61e30b81bd62dd4b27963b77fd67
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-25 17:08:50 +0200

    added static image to our attribute (works as normal html)

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 
 export default class Product extends Component {
   //adding count attribute for be able using it anywhere in our class
-  count = 0;
+  count = 5;
   render() {
     // const count = 0;
     const list = ['Item one ', 'Item two', 'Item three'];
@@ -11,21 +11,20 @@ export default class Product extends Component {
     return (
       <>
         <span className="m-2 text-info">Product Name</span>
-        <span className="m-2 badge bg-primary">
-          {this.count !== 0 && this.count}
-        </span>
+        <span className="m-2 badge bg-primary">{this.format()}</span>
         <button className="m-2 btn btn-sm btn-success">+</button>
         <button className="m-2 btn btn-sm btn-warning">-</button>
         <button className="m-2 btn btn-sm btn-danger">Delete</button>
         <ul>{listmap}</ul>
+        <img src="https://picsum.photos/200" alt="random foto" />
       </>
     );
   }
-  //   format() {
-  //     if (this.count === 0) {
-  //       return 'zero';
-  //     } else {
-  //       return this.count;
-  //     }
-  //   }
+  format() {
+    if (this.count === 0) {
+      return 'zero';
+    } else {
+      return this.count;
+    }
+  }
 }
