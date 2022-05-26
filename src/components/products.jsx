1322af168e180051b2b743d073ac87b26191e7ab
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-26 19:23:15 +0200

    used data from server for our state
    we have an array of objects

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,14 +1,26 @@
 import { Component } from 'react';
-import Product from './product';
 
 export default class Products extends Component {
+  state = {
+    products: [
+      {
+        id: 1,
+        count: 2,
+        productName: 'laptop',
+      },
+      {
+        id: 2,
+        count: 4,
+        productName: 'mobile',
+      },
+      {
+        id: 3,
+        count: 5,
+        productName: 'airpod',
+      },
+    ],
+  };
   render() {
-    return (
-      <>
-        <Product productName="laptop" />
-        <Product productName="mobile" />
-        <Product productName="airpod" />
-      </>
-    );
+    return <></>;
   }
 }
