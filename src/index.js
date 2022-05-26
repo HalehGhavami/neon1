04747d303472c2664c83f3c0443d81ddfba538a3
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-26 11:31:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import App from './App';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Product from './components/product';
+import Product from './components/functional/product';
 
 ReactDOM.render(<Product />, document.getElementById('root'));
