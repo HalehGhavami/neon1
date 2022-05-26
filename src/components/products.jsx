34cf2a6e732223729e3abdd920e21bc4605f9eab
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-26 12:43:08 +0200

    Create products.jsx
    to multi use a component

---
+++ src/components/products.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import Product from './product';
+
+export default class products extends Component {
+  render() {
+    return (
+      <div>
+        <Product />
+      </div>
+    );
+  }
+}
