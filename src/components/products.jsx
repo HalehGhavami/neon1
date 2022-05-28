1a2f9239e1c2a65bcf46761f7dec3785ab00d495
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-28 11:45:41 +0200

    found index of selected product by Id and increment its count

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -55,4 +55,13 @@ export default class Products extends Component {
     });
     this.setState({ products: newProducts });
   };
+
+  handelIncrement = (productId) => {
+    //make a copy of our products
+    const newProducts = [...this.state.products];
+    //search in newProducts array to find the index of selected productId
+    const index = newProducts.indexOf((product) => product.id === productId);
+    newProducts[index].count += 1;
+    this.setState({ products: newProducts });
+  };
 }
