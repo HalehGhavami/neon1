3f1be1c143d1aa441626a164550718275be48f27
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-28 16:57:26 +0200

    renamed context such a component

--- src/context/products.js
+++ src/context/products.js
@@ -1,6 +1,6 @@
 //Context is primarily used when some data needs to be accessible by many components at different nesting levels.
 import { createContext } from 'react';
 
-const productContext = createContext();
+const ProductContext = createContext();
 
-export default productContext;
+export default ProductContext;
