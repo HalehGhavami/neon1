d2a31fa7ad5f7eecd7c6ea5ce967a1a34d84e05f
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-28 16:53:49 +0200

    used createContext
    Context is primarily used when some data needs to be accessible by many components at different nesting levels.

--- src/context/products.js
+++ src/context/products.js
@@ -1,3 +1,4 @@
+//Context is primarily used when some data needs to be accessible by many components at different nesting levels.
 import { createContext } from 'react';
 
 const productContext = createContext();
