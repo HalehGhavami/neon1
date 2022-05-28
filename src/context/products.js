7798b9f02d31a9e905b786a0b96ab9a9304ce9b0
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-28 17:36:37 +0200

    Update products.js
    optional!

--- src/context/products.js
+++ src/context/products.js
@@ -1,6 +1,13 @@
 //Context is primarily used when some data needs to be accessible by many components at different nesting levels.
 import { createContext } from 'react';
 
-const ProductContext = createContext();
+const ProductContext = createContext({
+  //optional !! just to know what we have in context
+  products: [],
+  onDelete: () => {},
+  onIncrement: () => {},
+  onDecrement: () => {},
+  onReset: () => {},
+});
 
 export default ProductContext;
